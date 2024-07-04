6685b6fcdefe11a21ce90a6cccc1304958eb5d26
Kai-Zong <zong.k@northeastern.edu>
2024-07-04 14:38:10 -0700

    Added TextInput feature in App.js, where the text changes in textInput will be displayed in the next line using useState()

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Headers from './components/Headers';
+import { useState } from 'react';
 
 export default function App() {
 
+const [text, setText] = useState("");
 const appName = "My App";
   return (
     <View style={styles.container}>
       <Headers appName={appName}>
         <Text>something</Text>
         </Headers>
+
+        <TextInput 
+        style={{height:40}}
+        placeholder='Type here'
+        onChangeText={ newText => setText(newText)}></TextInput>
+        <Text>{text}</Text>
       <StatusBar style="auto" />
     </View>
   );
