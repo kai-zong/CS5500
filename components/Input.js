e84c47c3dd93b2f04ea3bfd9b9f9c66a74782a33
Kai-Zong <zong.k@northeastern.edu>
2024-07-09 13:37:47 -0700

    Refactor Input component to handle focus, display a message when not focused, and add submit button

--- components/Input.js
+++ components/Input.js
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { useState } from 'react';
 import { TextInput } from 'react-native';
 
 const Input = () => {
-
+  const handleConfirm = () => {console.log(text)}
   const[isFocused, setIsFocused] = useState(false)
+  const [text, setText] = useState("");
   return (
     <View>
     <TextInput 
@@ -13,8 +14,10 @@ const Input = () => {
     placeholder='Type here'
     onFocus={()=>setIsFocused(true)}
     onBlur={()=>setIsFocused(false)}
+    onChangeText={(text)=>setText(text)}
     ></TextInput>
     {!isFocused && <Text>Thank you</Text>}
+    <Button title='Submit' onPress={()=>handleConfirm(text)}/>
     </View>
   )
 }
