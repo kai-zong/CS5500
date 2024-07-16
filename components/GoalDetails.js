3c5f5150782d6b60df7efeeb5b39c4f10bd9d3ad
kai-zong <zong.k@northeastern.edu>
2024-07-16 14:26:49 -0700

    refactor: Update GoalDetails component with button for navigation push

--- components/GoalDetails.js
+++ components/GoalDetails.js
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from "react-native";
+import React from "react";
 
-export default function GoalDetails({ navigation, route}) {
+export default function GoalDetails({ navigation, route }) {
   return (
     <View>
-      <Text>You are seeing the details of the goal with text {route.params.goal.text} and id {route.params.goal.id}</Text>
+      <Text>
+        You are seeing the details of the goal with text{" "}
+        {route.params.goal.text} and id {route.params.goal.id}
+      </Text>
+      <Button title="More" onPress={()=>{navigation.push("Details")}}/>
     </View>
-  )
+  );
 }
